0b5caa6ccb189486e9ab089c2a66113ed1415e68
mscappini <mark.scappini@gmail.com>
2015-04-17 22:42:57 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+OptBot Configuration Loader
+===========================
+Description
+--
+Configuration loader for the optbot services.
+
+Usage
+--
+
+`require('@optbot/cfgloader')`
---
+++ cfgloader.js
@@ -0,0 +1,37 @@
+﻿/*
+ * Copyright (c) 2015 Marshall Farrier, Robert Rodrigues, Mark Scappini
+ * license http://opensource.org/licenses/MIT
+ */
+
+(function () {
+    'use strict';
+
+    var fs = require('fs'),
+        path = require('path'),
+        nconf = require('nconf');
+    
+    /* The order in which you attach these configuration sources determines their priority in the hierarchy. */
+    
+    // 1. Command line args
+    //nconf.argv({
+    //    'host': { alias: 'web:host' },
+    //    'port': { alias: 'web:port' }
+    //});
+    
+    // 2. Environment variables
+    //nconf.env();
+    
+    // 3. Environment-specific config values
+    if (process.env && process.env.NODE_ENV) {
+        var envConfig = path.join(__dirname, 'config.' + process.env.NODE_ENV + '.json');
+        if (fs.existsSync(envConfig)) {
+            nconf.file(envConfig);
+        }
+    }
+    
+    // 4. Default config values
+    var defaultConfig = path.join(__dirname, 'config.json');
+    nconf.file(defaultConfig);
+    
+    module.exports = nconf;
+})();
